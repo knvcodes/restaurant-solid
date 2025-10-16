2b6601ae73e2105d88235c13ba064233446ac52f
krunal vaghadia <krunalvaghadia@gmail.com>
2025-10-16 23:13:52 +0530

    removed incorrect search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -25,43 +25,10 @@ export default function SearchBar() {
   ];
 
   return (
-    <>
-      <Search
-        triggerMode="focus"
-        debounceOptionsMillisecond={800}
-        options={optionsS}
-        class="mb-12"
-        onInputChange={(query) => {
-          console.log(query);
-
-          setSearch(query);
-        }}
-        onChange={(result) => setEmoji(result)}
-        placeholder="Search a restaurant..."
-        itemComponent={(props) => (
-          <Search.Item item={props.item} class="search__item">
-            <Search.ItemLabel>{props.item.rawValue.label}</Search.ItemLabel>
-          </Search.Item>
-        )}
-      >
-        <Search.Control class="search__control" aria-label="Emoji">
-          <Search.Input class="search__input rounded-xl p-2 px-4 border border-gray-200/60 w-full" />
-        </Search.Control>
-        <Search.Portal>
-          <Search.Content
-            class="search__content"
-            onCloseAutoFocus={(e) => e.preventDefault()}
-          >
-            <Search.Listbox class="search__listbox" />
-            <Search.NoResult class="search__no_result">
-              😬 No emoji found
-            </Search.NoResult>
-          </Search.Content>
-        </Search.Portal>
-      </Search>
-      <div class="result__content">
-        {/* Emoji selected: {emoji()?.emoji} {emoji()?.name} */}
-      </div>
-    </>
+    <input
+      type="text"
+      placeholder="Search your restaurants..."
+      class="mb-12 search__input rounded-xl p-2 px-4 border border-gray-200/60 w-full"
+    />
   );
 }
