640c468f6a537f55548442fd70d9fe38bf4d1572
krunal vaghadia <krunalvaghadia@gmail.com>
2025-11-03 17:28:26 +0530

    added card border below

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,31 +3,37 @@ import { FiStar } from "solid-icons/fi";
 import { generateRandomImageUrl } from "../utils/helpers";
 import { randomImageUrls } from "../utils/staticData";
 import { ICardProps } from "../types";
+import { Separator } from "@kobalte/core/separator";
 
 export default function Card(props: ICardProps) {
   const { name = "Brooklynn Stake House", address, city } = props;
   return (
-    <div class="card relative">
-      <div class="absolute inset-0">
-        <img
-          src={randomImageUrls[generateRandomImageUrl(randomImageUrls.length)]}
-          alt="Avatar"
-          class="h-full w-full object-cover"
-        />
-      </div>
+    <>
+      <div class="card relative">
+        <div class="absolute inset-0">
+          <img
+            src={
+              randomImageUrls[generateRandomImageUrl(randomImageUrls.length)]
+            }
+            alt="Avatar"
+            class="h-full w-full object-cover"
+          />
+        </div>
 
-      <div class="card-details">
-        <h2 class="text-3xl font-semibold">{name}</h2>
-        <p class="text-sm mt-2 text-black/80">{address}</p>
-        <p class="text-sm underline mt-4 font-medium">{city}</p>
-        <div class="flex gap-2 my-4">
-          <FaSolidStar />
-          <FaSolidStar />
-          <FaSolidStar />
-          <FaSolidStar />
-          <FiStar />
+        <div class="card-details">
+          <h2 class="text-3xl font-semibold">{name}</h2>
+          <p class="text-sm mt-2 text-black/80">{address}</p>
+          <p class="text-sm underline mt-4 font-medium">{city}</p>
+          <div class="flex gap-2 my-4">
+            <FaSolidStar />
+            <FaSolidStar />
+            <FaSolidStar />
+            <FaSolidStar />
+            <FiStar />
+          </div>
         </div>
       </div>
-    </div>
+      <Separator class="separator" />
+    </>
   );
 }
