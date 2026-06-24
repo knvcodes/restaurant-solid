9698683c3f1f017d4cb6270c72001770f5bf2175
knvcodes <knvcodes@gmail.com>
2026-06-24 14:50:10 +0530

    feat: added styling for account menu along with logout

--- src/components/Header/AccountMenu.tsx
+++ src/components/Header/AccountMenu.tsx
@@ -1,29 +1,54 @@
 import { createEffect, createSignal, Show } from "solid-js";
-import { userStore } from "../../store/userStore";
+import { handleLogout, userStore } from "../../store/userStore";
 import { restaurant_placeholder } from "../../assets/assets";
 import { FaSolidCircleUser } from "solid-icons/fa";
+import { useLocation } from "@solidjs/router";
 
 interface AccountMenuProps {}
 
 export function AccountMenu(props: AccountMenuProps) {
+  const location = useLocation();
+
   const [isMenuOpen, setisMenuOpen] = createSignal(false);
+  const [bgClass, setbgClass] = createSignal("bg-gray-800/80");
+
+  // dynamic styling
+  createEffect(() => {
+    const pathname = location.pathname;
+
+    if (pathname == "/") {
+      setbgClass("bg-gray-800/60 hover:bg-gray-700/60");
+    } else {
+      setbgClass("bg-white/80 hover:bg-gray-100/80");
+    }
+  });
 
   return (
-    <div class="horizontal-list gap-4 items-center cursor-pointer">
-      <Show
-        when={userStore.avatar}
-        fallback={<FaSolidCircleUser font-size="30" />}
+    <div class="min-w-[var(--account-menu-min-w)]">
+      <div
+        class="horizontal-list gap-4 items-center cursor-pointer relative"
+        onclick={() => setisMenuOpen((p) => !p)}
       >
-        <img
-          src={userStore.avatar ?? restaurant_placeholder}
-          alt=""
-          referrerpolicy="no-referrer" // Key fix!
-          width={40}
-          height={40}
-          class="rounded-full"
-        />
+        <Show
+          when={userStore.avatar}
+          fallback={<FaSolidCircleUser font-size="30" />}
+        >
+          <img
+            src={userStore.avatar ?? restaurant_placeholder}
+            alt=""
+            referrerpolicy="no-referrer" // Key fix!
+            width={40}
+            height={40}
+            class="rounded-full"
+          />
+        </Show>
+        <div>{userStore.name}</div>
+      </div>
+      <Show when={isMenuOpen()}>
+        <div class={`account-menu ${bgClass()}`}>
+          <div onclick={handleLogout}>Logout</div>
+        </div>
       </Show>
-      <div>{userStore.name}</div>
     </div>
   );
 }
