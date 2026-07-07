5ab1c55ce8ad83a68fd3b7d9b9a458f9256a6579
knvcodes <knvcodes@gmail.com>
2026-07-07 17:18:37 +0530

    feat: added frontend caching in restaurant listing

--- src/service/restaurants/customer.service.ts
+++ src/service/restaurants/customer.service.ts
@@ -1,18 +1,20 @@
-import { toastActions } from "../../store/toastStore";
+import { createQuery } from "@tanstack/solid-query";
 import { IResponse, IRestaurant } from "../../types";
 import api from "../../utils/axios";
 import { getErrorMessage, isEmpty, showToastErrors } from "../../utils/helpers";
+import { Accessor } from "solid-js";
+import { TIME_10_MIN, TIME_5_MIN } from "../../utils/service.config";
 
 export const restaurantListing = async (search?: string) => {
   try {
     let url = "/restaurants/list";
 
+    // add search params if present
     if (!isEmpty(search)) {
       url += `?search=${search}`;
     }
 
     const response: IResponse<IRestaurant[]> = await api.get(url);
-
     return response.data.data;
   } catch (error) {
     const errorMessage = getErrorMessage(error);
@@ -21,6 +23,16 @@ export const restaurantListing = async (search?: string) => {
       showToastErrors(errorMessage);
     }
 
+    // if error send empty list
     return [];
   }
 };
+
+// service.ts
+export const useRestaurants = (search: Accessor<string>) =>
+  createQuery(() => ({
+    queryKey: ["restaurants", "list", search()],
+    queryFn: () => restaurantListing(search()),
+    staleTime: TIME_5_MIN,
+    gcTime: TIME_10_MIN,
+  }));
---
+++ src/utils/service.config.ts
@@ -0,0 +1,4 @@
+const TIME_10_MIN = 1000 * 60 * 10;
+const TIME_5_MIN = 1000 * 60 * 5;
+
+export { TIME_10_MIN, TIME_5_MIN };
